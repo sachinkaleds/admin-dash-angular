d1861afd3dcd479fc699a4fa7053e305ea2cf75e
Sachin Kale <sachinkaleds@gmail.com>
2022-07-23 12:28:20 +0530

    small updates

--- src/app/theme/widgets/branding.component.ts
+++ src/app/theme/widgets/branding.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <a class="matero-branding" href="/">
       <img src="./assets/images/matero.png" class="matero-branding-logo-expanded" alt="logo" />
-      <span class="matero-branding-name">MATERO</span>
+      <span class="matero-branding-name">ADMIN</span>
     </a>
   `,
 })
